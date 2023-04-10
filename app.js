4a313bc512dd66ad1b2c1a12d286bb99256ce960
fantasseryoga <69531863+fantasseryoga@users.noreply.github.com>
2023-04-10 16:19:11 +0300

    cors

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ const http = require('http').createServer(app)
 const chat = require("./socket/chat.js")(http)
 const Chat = require("./models/Chat.js")
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(express.json({ extended: true, limit: "50mb" }))
 app.use('/api/chats', require('./routes/chat.routes'))
 app.use('/api/auth', require('./routes/auth.routes'))
